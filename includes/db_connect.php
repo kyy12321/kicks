c37b6584fd694bffd5396a34c4a3df9e66f5daa8
mbhornido <angelohornido2@gmail.com>
2025-05-18 10:31:29 +0800

    Update db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,8 +1,13 @@
 <?php
+// $servername = "localhost";
+// $username = "root";
+// $password = ""; // Typically root has no password in XAMPP by default
+// $dbname = "kicksf";
+
 $servername = "localhost";
-$username = "root";
-$password = ""; // Typically root has no password in XAMPP by default
-$dbname = "kicksf";
+$username = "u866427573_kicks";
+$password = "@Qetu1357"; // Typically root has no password in XAMPP by default
+$dbname = "u866427573_kicks";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
